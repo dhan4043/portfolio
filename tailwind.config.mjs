25759fb17caf687b3dc3f9a7066b01a83b142eb2
dhan4043 <dhan4043@gmail.com>
2024-06-13 18:19:35 -0400

    added custom styles and fonts to styles/global

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const tailwind_theme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      fontFamily: {
+        sans: ['Ia Writer Mono', ...tailwind_theme.fontFamily.sans],
+        mono: ['Ia Writer Mono', ...tailwind_theme.fontFamily.mono],
+      },
+    },
 	},
 	plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     themes: false, // light and dark theme only 
-    logs: false
+    logs: false,
+    styled: false  // disable daisyUI colors and design decisions
   }
 }
